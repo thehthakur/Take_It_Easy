c07b1c9951fe38c351babd889e965615069d7ba0
Kaushik Bhowmick <56549069+kaushik2107-bit@users.noreply.github.com>
2024-11-24 13:56:01 +0530

    merge conflict

--- src/cost_model/graph_traversal.py
+++ src/cost_model/graph_traversal.py
@@ -1,21 +1,23 @@
-import subprocess
 import os
 import sys
 import argparse
+import onnx
+from onnx import ModelProto
+from typing import List, Any
+import heapq
 
 rootPath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
 sys.path.insert(0, rootPath)
 
-def serve_onnx_to_netron(path):
-    try:
-        subprocess.run(['netron', path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to open Netron: {e}")
+def adjacency_graph(modeL: ModelProto):
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Serve an ONNX model to Netron.")
     parser.add_argument("path", type=str, help="Path to the ONNX model file from root folder")
     args = parser.parse_args()
+
     model_path = os.path.join(rootPath, args.path)
-    serve_onnx_to_netron(model_path)
-    # print(model_path)
+    model = onnx.load_model(model_path)
+
+    adjacency_graph(model)
